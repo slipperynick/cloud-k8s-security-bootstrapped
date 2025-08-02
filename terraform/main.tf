fc653e93d0b7c20a9ba5c5cf3864193c64a1df84
slipperynick <nick@badclick.org>
2025-08-02 12:54:03 +0200

    azurerm config for oidc part 2

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,10 +6,9 @@ provider "azurerm" {
     }
   }
   use_cli              = false
-  // use_oidc must be explicitly set to true when using multiple configurations.
   use_oidc             = true
-  client_id_file_path  = var.tfc_azure_dynamic_credentials.default.client_id_file_path
-  oidc_token_file_path = var.tfc_azure_dynamic_credentials.default.oidc_token_file_path
+  # client_id_file_path  = var.tfc_azure_dynamic_credentials.default.client_id_file_path
+  # oidc_token_file_path = var.tfc_azure_dynamic_credentials.default.oidc_token_file_path
   subscription_id      = "6bf22291-b263-47df-8576-e6418cee5e4a"
   tenant_id            = "8a2e1cd7-f42b-43d9-abc5-f6061c3b1545"
 }
