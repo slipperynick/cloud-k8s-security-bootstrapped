043066af9ad4f5ac4600be9ed54d39d653076c89
slipperynick <nick@badclick.org>
2025-08-02 18:54:34 +0200

    terraform formatting

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,8 +5,8 @@ provider "azurerm" {
       recover_soft_deleted_key_vaults = true
     }
   }
-  use_cli              = false
-  use_oidc             = true
-  subscription_id      = "6bf22291-b263-47df-8576-e6418cee5e4a"
-  tenant_id            = "8a2e1cd7-f42b-43d9-abc5-f6061c3b1545"
+  use_cli         = false
+  use_oidc        = true
+  subscription_id = "6bf22291-b263-47df-8576-e6418cee5e4a"
+  tenant_id       = "8a2e1cd7-f42b-43d9-abc5-f6061c3b1545"
 }
